7b3be8b5bbc39c49947876ef768cd59ae39866cf
santramarketonlineshoppingmall <mansasingh7109@gmail.com>
2026-05-19 03:23:03 +0530

    Add my website file
    Add my file for website santramarketshoppingmall

---
+++ secrets.js
@@ -0,0 +1,19 @@
+// secrets.js - Firebase + ImgBB + Admin Details
+const firebase_config = {
+  apiKey: "AIzaSyApXIGoX071cYEvGbfhBF69DB9Kv5YlSMA",
+  authDomain: "santramarketshoppingmall.firebaseapp.com",
+  projectId: "santramarketshoppingmall",
+  storageBucket: "santramarketshoppingmall.firebasestorage.app",
+  messagingSenderId: "398490252924",
+  appId: "1:398490252924:web:d1b6348b549183b93b7bf9",
+  measurementId: "G-ZW1ZR8HETY"
+};
+
+// Dono naam se export - Capital + Small
+export const FIREBASE_CONFIG = firebase_config;
+export const firebaseConfig = firebase_config; 
+
+// Admin Details
+export const ADMIN_EMAIL = "mansasingh7109@gmail.com";
+export const ADMIN_NAME = "Manisha Singh";
+export const ADMIN_MOBILE = "9829508335";
