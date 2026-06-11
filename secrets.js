4587e5449f0630e1ab80d9dcb7b8140821d193ed
santramarketonlineshoppingmall <mansasingh7109@gmail.com>
2026-06-11 13:11:36 +0530

    Add my file
    Add my file

--- secrets.js
+++ secrets.js
@@ -1,11 +1,5 @@
-// Google wali 2 line delete kar de
-// const GOOGLE_API_KEY = '...'; DELETE THIS
-// const GOOGLE_SEARCH_ENGINE_ID = '...'; DELETE THIS
-
-// secrets.js - 100% Free Version, No Billing Needed
-
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyApXIGoX071cYEvGbfhBF69DB9Kv5YlSMA",
+   const firebaseConfig = {
+     apiKey: "AIzaSyApXIGoX071cYEvGbfhBF69DB9Kv5YlSMA",
   authDomain: "santramarketshoppingmall.firebaseapp.com",
   databaseURL: "https://santramarketshoppingmall-default-rtdb.firebaseio.com/",
   projectId: "santramarketshoppingmall",
@@ -13,13 +7,9 @@ const FIREBASE_CONFIG = {
   messagingSenderId: "398490252924",
   appId: "1:398490252924:web:d1b6348b549183b93b7bf9",
   measurementId: "G-ZW1ZR8HETY"
-};
+   };
 
-// Backup ke liye chote wala bhi
-const firebaseConfig = FIREBASE_CONFIG;
-
-const BUSINESS_CONFIG = {
-  WHATSAPP_NUMBER: "918769171078",
-  EMAIL: "mansasingh7109@gmail.com",
-  EMAIL_2: "santramarketshoppingmall@gmail.com"
-  };
+   const BUSINESS_CONFIG = {
+     WHATSAPP_NUMBER: "918769171078",
+     EMAIL: "mansasingh7109@gmail.com"
+   };
