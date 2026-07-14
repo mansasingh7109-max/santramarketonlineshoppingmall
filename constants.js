1bc14d855012c9e6b6f5e71f89b729c6d6983c42
santramarketonlineshoppingmall <mansasingh7109@gmail.com>
2026-07-15 00:04:22 +0530

    Add my file
    Add my file

---
+++ constants.js
@@ -0,0 +1,9 @@
+// constants.js - SIRF EK BAAR DECLARE HOGA
+const CART_KEY = "santraMallCart_v2";
+const MYCHOICE_KEY = "santraMallMyChoice_v2";
+
+// Window pe bhi set kar do taaki sab use kar sake
+window.CART_KEY = CART_KEY;
+window.MYCHOICE_KEY = MYCHOICE_KEY;
+
+console.log("✅ Constants loaded - CART_KEY:", CART_KEY);
